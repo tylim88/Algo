2ce63151c55060496c8adff3212227d0e1002ad7
tylim <5227509+tylim88@users.noreply.github.com>
2019-02-10 14:53:53 +0800

    Create Sort According to Properties.js

---
+++ Sort According to Properties.js
@@ -0,0 +1,31 @@
+const item = ['ball', 'box', 'ball', 'ball', 'box', 'x']
+const price = [2, 2, 2, 2, 2, 5]
+const weight = [1, 2, 3, 1, 2, 2]
+
+const itemIndex = {}
+const itemPrice = {}
+const itemWeight = {}
+
+item.forEach((element, index) => {
+    const priceProp = 'price ' + price[index]
+    const weightProp = 'weight ' + weight[index]
+    if (!itemIndex[element]) {
+        itemIndex[element] = {
+            [priceProp]: { [weightProp]: [] },
+        }
+    } else if (!itemIndex[element][priceProp]) {
+        itemIndex[element][priceProp] = {
+            [weightProp]: [],
+        }
+    } else if (!itemIndex[element][priceProp][weightProp]) {
+        itemIndex[element][priceProp][weightProp] = []
+    }
+
+    itemIndex[element][priceProp][weightProp].push(index)
+    itemPrice[element] = (itemPrice[element] || 0) + price[index]
+    itemWeight[element] = (itemWeight[element] || 0) + weight[index]
+})
+
+console.log('index', itemIndex)
+console.log('price', itemPrice)
+console.log('weight', itemWeight)
